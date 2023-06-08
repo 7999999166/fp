172bba1c82096b201370f8620a175defa89fc7b7
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-06-09 02:25:16 +0530

    aaxa

--- src/routes/[items]/+page.svelte
+++ src/routes/[items]/+page.svelte
@@ -6,7 +6,7 @@
   
 
 
-  if (items == 'Dustbins'){ myProducts = myData.Dustbins ; }
+  if (items == 'dustbins'){ myProducts = myData.Dustbins ; }
   if (items == 'Containers'){ myProducts = myData.Containers ; }
   if (items == 'Planters'){ myProducts = myData.Planters ; }
   if (items == 'Buckets'){ myProducts = myData.Buckets ; }
