86b8695d5651823f97aa0233b03d81dd722f2d4b
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-11-12 01:05:06 +0530

    amsms

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,5 +1,5 @@
 // Docs on event and context https://docs.netlify.com/functions/build/#code-your-function-2
 const handler = 
-async function (req, context){return new response ("hello kanik") ;}
+async function (req, context){return new req ("hello kanik") ;}
 
 module.exports = { handler }
