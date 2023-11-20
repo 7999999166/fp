8116e31a3e00485b8b3818126e1bdd1bd2bb0380
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-11-20 13:40:02 +0530

    check

--- src/routes/products/+page.svelte
+++ src/routes/products/+page.svelte
@@ -1,7 +1,5 @@
 <script>
 
-    
-
     import {db} from '../../db.js';
     import {orderTotal} from "../../store.js";
     import {cartQtyTotal} from "../../store2.js";
@@ -35,7 +33,7 @@
     <div class="container py-4 py-xl-5">
     <div class="row mb-5">
         <div class="col-md-8 col-xl-6 text-center mx-auto">
-            <h2 on:click={help}>Product Catagories</h2>
+            <h2>Product Catagories</h2>
             <p>Curae hendrerit donec commodo hendrerit egestas tempus, turpis facilisis nostra nunc. Vestibulum dui eget ultrices.</p>
         </div>
     </div>
