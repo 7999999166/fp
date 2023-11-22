4b6856e14bbdce46cdb1d3e1d748c647dc74021b
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-11-22 22:52:31 +0530

    checkhash

--- netlify/functions/hello-world/hello-world.js
+++ netlify/functions/hello-world/hello-world.js
@@ -1,11 +1,15 @@
 
-var myName ={name: process.env.KANJURA}
+var crypto = require('crypto');
 
 
 exports.handler = async function (event){
+
+
+     var name = 'braitsch';
+     var hash = crypto.createHash('sha512').update(name).digest('hex');
    return {
            statusCode : 200 ,
-           body : JSON.stringify(myName) 
+           body : JSON.stringify(hash) 
          };
 }
 
