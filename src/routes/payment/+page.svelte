03123b89a7e29dc47bbdbf42829354d49e549c8b
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-11-23 00:31:50 +0530

    ghj

---
+++ src/routes/payment/+page.svelte
@@ -0,0 +1,17 @@
+<script>
+    async function aaa(){var xxx = new Promise (async function(resolve){var zash = await axios.get('https://akhilsteel.in/.netlify/functions/hello-world') ;  resolve(zash); }); return xxx ;}
+    async function myMtain(){var hash = await aaa() ;
+                             console.log(hash);}
+
+
+  
+</script>
+
+<div>
+   <h1>Hello Payment</h1>
+   <button class="btn btn-warning" on:click={myMtain}>EaseBuzz-Checking</button>
+</div>
+
+<style>
+   
+</style>
