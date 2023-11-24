297ffacdbe21616c486d3f838ead28742dd099db
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-11-24 10:46:48 +0530

    logincheck

--- src/routes/login/+page.svelte
+++ src/routes/login/+page.svelte
@@ -1,20 +1,32 @@
 <script>
-    var phone ;
+    var userName ;
     var password ;
+    var jtoken ;
+    var user ;
+
+
+  async function daru(){
+    axios({
+            method:"post",
+            url:"https://starlightsteel.in/api/auth/local",
+            data:{
+                   identifier: userName,
+                   password: password,
+                 }
+   })
+  .then(response => {
+
+    console.log('User profile', response.data.user);
+    console.log('User token', response.data.jwt);
+    jtoken = response.data.jwt ;
+    user = response.data.user ;
+    
+  })
+}
 
 
 
-async function jwala(){
 
- var info =  await axios.post('https://strapipay-production.up.railway.app/api/auth/local', {
-    identifier: phone,
-    password: password
-    } 
-   )
-     console.log('User profile', info.data.user);
-     console.log('User token', info.data.jwt);
-
-    }
 
   
 </script>
@@ -36,9 +48,9 @@ async function jwala(){
                                 <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6zm2-3a2 2 0 1 1-4 0 2 2 0 0 1 4 0zm4 8c0 1-1 1-1 1H3s-1 0-1-1 1-4 6-4 6 3 6 4zm-1-.004c-.001-.246-.154-.986-.832-1.664C11.516 10.68 10.289 10 8 10c-2.29 0-3.516.68-4.168 1.332-.678.678-.83 1.418-.832 1.664h10z"></path>
                             </svg></div>
                          <div>
-                            <div class="mb-3"><input bind:value={phone} class="form-control" type="email" name="number" placeholder="Phone number" /></div>
-                            <div class="mb-3"><input bind:value={password} class="form-control" type="password" name="password" placeholder="Password" /></div>
-                            <div class="mb-3"><button class="btn btn-primary d-block w-100"  on:click={jwala}>Sign In</button></div>
+                            <div class="mb-3"><input bind:value={userName} class="form-control" type="email"  placeholder="User Name or Email id" /></div>
+                            <div class="mb-3"><input bind:value={password} class="form-control" type="password" placeholder="Password" /></div>
+                            <div class="mb-3"><button class="btn btn-primary d-block w-100"  on:click={daru}>Sign In</button></div>
                             <a href="/signUp">Need to Register ?</a>
                         </div>
                     </div>
