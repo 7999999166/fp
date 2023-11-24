3e654795af6e990cf2c161fddb4e98c948feb277
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-11-24 12:02:56 +0530

    sjxbksjxb

--- src/db.js
+++ src/db.js
@@ -2,7 +2,6 @@
 
    const db = new Dexie('myDatabase');
  
-
-   db.version(1).stores({ order: '++id , Productname , Price , Quantity , Amount ,Count ' },{ userInfo: '++id , jwt , userId , userName'});
-
+   db.version(1).stores({ order: '++id , Productname , Price , Quantity , Amount ,Count ' },);
+   db.version(2).stores({ userInfo: '++id , jwt , userId , userName'});
    export {db};
