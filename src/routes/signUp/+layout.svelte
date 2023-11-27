f6f40a527b48eb509218cbbeb7c47224a1d1107a
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-11-27 16:07:07 +0530

    helo

--- src/routes/signUp/+layout.svelte
+++ src/routes/signUp/+layout.svelte
@@ -6,11 +6,11 @@
   <slot></slot>
   <footer class="text-center bg-dark w3-bottom ">
     <div class="container text-white py-4 py-lg-5">
-        <ul class="list-inline">
+        <!-- <ul class="list-inline">
               <li class="list-inline-item me-4"><a class="link-light jiji" href="/termsCondition">Terms & Conditions</a></li>
             <li class="list-inline-item me-4"><a class="link-light jiji" href="/privacyPolicy">Privacy Policy</a></li>
             <li class="list-inline-item"><a class="link-light jiji" href="/returnRefund">Refund & Return</a></li> 
-        </ul>
+        </ul> -->
         <ul class="list-inline">
             <li class="list-inline-item me-4"><svg class="bi bi-facebook text-light" xmlns="http://www.w3.org/2000/svg" width="1em" height="1em" fill="currentColor" viewBox="0 0 16 16">
                     <path d="M16 8.049c0-4.446-3.582-8.05-8-8.05C3.58 0-.002 3.603-.002 8.05c0 4.017 2.926 7.347 6.75 7.951v-5.625h-2.03V8.05H6.75V6.275c0-2.017 1.195-3.131 3.022-3.131.876 0 1.791.157 1.791.157v1.98h-1.009c-.993 0-1.303.621-1.303 1.258v1.51h2.218l-.354 2.326H9.25V16c3.824-.604 6.75-3.934 6.75-7.951z"></path>
