1806b76530ee20944374018410b491faeec140c3
Kanik Goyal <63117613+7999999166@users.noreply.github.com>
2023-12-02 13:54:48 +0530

    chuiuy

--- netlify/functions/payment-function.js
+++ netlify/functions/payment-function.js
@@ -1,4 +1,5 @@
 var axios = require('axios');
+var sha512 = require('js-sha512');
 var crypto = require('crypto');
 
 
@@ -11,12 +12,13 @@ var firstname = 'sandeep' ;
 var phone = '9090909090';
 var email = 'mahesh@gmail.com' ;
 var salt = process.env.SALT ;
-var myValue = key+txnid+amount+productinfo+firstname+phone+email+salt ;
+var myValue = key+'|'+txnid+'|'+amount+'|'+productinfo+'|'+firstname+'|'+phone+'|'+email+'|'+'|'+'|'+'|'+'|'+'|'+'|'+'|'+'|'+'|'+'|'+salt ;
 var hash = crypto.createHash('sha512').update(myValue).digest('hex');
+var zash = sha512.sha512(myValue);
 
 
 
-exports.handler = async function (event,token){
+exports.handler = async function (event,res){
 
 const encodedParams = new URLSearchParams();
 encodedParams.set('key', key);
@@ -28,7 +30,7 @@ encodedParams.set('phone', phone);
 encodedParams.set('email', email);
 encodedParams.set('surl', 'https://akhilsteel.in');
 encodedParams.set('furl', 'https://akhilsteel.in/payment');
-encodedParams.set('hash', hash);
+encodedParams.set('hash', zash);
 
 
 const options = {
@@ -41,11 +43,11 @@ const options = {
   data: encodedParams,
 };
 
- token = await axios.request(options);
+ res = await axios.request(options);
 
  return {
            statusCode : 200 ,
-           body : JSON.stringify(token) 
+           body : JSON.stringify(zash) 
          };
 
 
